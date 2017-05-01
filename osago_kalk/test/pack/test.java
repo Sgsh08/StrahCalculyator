9f7ba5893014c3c0c10e78cb7195931ead73fb14
MrArtur0074 <smiteartur01@mail.ru>
2017-05-01 16:01:49 +0500

    Создание тестов для программы.

--- osago_kalk/test/pack/test.java
+++ osago_kalk/test/pack/test.java
@@ -10,11 +10,35 @@ public class test {
 
 	@Test
 	public void test1() {
+		// Грузовой автомобиль (<16 т.)
+		// От 120 до 150 л.с.
+		// До 22 лет, стаж выше 3 лет
+		// Без ограничения
+		float k1=(float)1.4,k2=(float)1.6,k3=2025,k4=(float)1.8;
+		int s;
+		//s - значение из функции sums, расположенной в классе klient
+		s=klient.sums(k1,k2,k3,k4);
+		// tests - итоговое значение
+		int tests=14697;
+		// итоговая сверка
+		assertEquals(tests,s);
 		
 	}
 	
 	@Test
 	public void test2() {
+		// Грузовой автомобиль (>=16 т.)
+		// >150 л.с.
+		// До 22 лет, стаж до 3 лет
+		// Ограниченный список водителей
+		float k1=(float)1.6,k2=(float)1.8,k3=3240,k4=1;
+		int s;
+		//s - значение из функции sums, расположенной в классе klient
+		s=klient.sums(k1,k2,k3,k4);
+		// tests - итоговое значение
+		int tests=16796;
+		// итоговая сверка
+		assertEquals(tests,s);
 		
 	}
 }
